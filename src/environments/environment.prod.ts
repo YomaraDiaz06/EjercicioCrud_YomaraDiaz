a8af38aff35f60801fae82926817434be091a721
YomaraDiaz06 <yomara.diaz@epn.edu.ec>
2021-07-06 16:19:10 -0500

    ejercicio crud

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+  apiKey: "AIzaSyBY5RoLKU-wK5xXwYL6Tu3B06lJ3wgso8M",
+  authDomain: "crud2-aa809.firebaseapp.com",
+  projectId: "crud2-aa809",
+  storageBucket: "crud2-aa809.appspot.com",
+  messagingSenderId: "107818927599",
+  appId: "1:107818927599:web:3c0466d6a58ba98c63a21a",
+  measurementId: "G-ZVX8EVRRCM"
+}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBY5RoLKU-wK5xXwYL6Tu3B06lJ3wgso8M",
+    authDomain: "crud2-aa809.firebaseapp.com",
+    projectId: "crud2-aa809",
+    storageBucket: "crud2-aa809.appspot.com",
+    messagingSenderId: "107818927599",
+    appId: "1:107818927599:web:3c0466d6a58ba98c63a21a",
+    measurementId: "G-ZVX8EVRRCM"
+  }
 };
 
 /*
